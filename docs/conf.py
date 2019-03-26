cdedf65bcef496fa3c37f46a4a38e75eaa4dfa6a
g00dvin <goodvin.l4@gmail.com>
2019-03-26 10:11:18 +0300

    no message

--- docs/conf.py
+++ docs/conf.py
@@ -70,7 +70,7 @@ pygments_style = 'sphinx'
 
 
 # -- Options for HTML output -------------------------------------------------
-
+import sphinx_rtd_theme
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
